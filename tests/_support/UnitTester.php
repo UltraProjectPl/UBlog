3c31c7ab7ec82fd6aa3500627bdc01254bd0492a
UltraProjectPl <jakub.sumara09@gmail.com>
2019-10-30 14:15:51 +0100

    Installed codeception

---
+++ tests/_support/UnitTester.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Tests\_generated\UnitTesterActions;
+use Codeception\Actor;
+
+class UnitTester extends Actor
+{
+    use UnitTesterActions;
+}
