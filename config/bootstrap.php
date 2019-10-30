9c936461da9b0de64d669ebd76221e925cfbd2b7
UltraProjectPl <jakub.sumara09@gmail.com>
2019-10-30 14:49:05 +0100

    Style fixed

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,11 +4,11 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
+if (true === is_array($env = @include dirname(__DIR__) . '/.env.local.php')) {
     foreach ($env as $k => $v) {
         $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
     }
-} elseif (!class_exists(Dotenv::class)) {
+} elseif (false === class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } else {
     (new Dotenv(false))->loadEnv(dirname(__DIR__) . '/.env');
