ce5ae12dfbf0bfb2de7d235b45d71d9bb0908aa3
UltraProjectPl <jakub.sumara09@gmail.com>
2019-11-10 20:28:36 +0100

    ComplierPass form

--- src/SharedKernel/Infrastructure/Bundle/SharedKernelBundle.php
+++ src/SharedKernel/Infrastructure/Bundle/SharedKernelBundle.php
@@ -3,9 +3,14 @@ declare(strict_types=1);
 
 namespace App\SharedKernel\Infrastructure\Bundle;
 
+use App\SharedKernel\Infrastructure\Bundle\DependencyInjection\CompilerPass\FormMapCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SharedKernelBundle extends Bundle
 {
-
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new FormMapCompilerPass());
+    }
 }
